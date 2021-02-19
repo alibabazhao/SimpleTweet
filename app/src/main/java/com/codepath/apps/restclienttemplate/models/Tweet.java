98b59ecd8ff57433755f949cc74e4f6a7034587a
Ali <1130aliz@gmail.com>
2021-02-19 12:35:54 -0500

    load more data

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -13,11 +13,15 @@ public class Tweet {
     public String createdAt;
     public User user;
 
+    public long id;
+
     public static Tweet fromJason(JSONObject jsonObject) throws JSONException {
         Tweet tweet=new Tweet();
         tweet.body=jsonObject.getString("text");
         tweet.createdAt=jsonObject.getString("created_at");
         tweet.user=User.fromJson(jsonObject.getJSONObject("user"));
+
+        tweet.id=jsonObject.getLong("id");
         return tweet;
     }
 
